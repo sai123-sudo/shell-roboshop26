db640f19aff205ff08bb5029499bc99a3778f0a7
sai123-sudo <ls0122207@gmail.com>
2026-04-22 12:45:50 +0530

    hello world started

--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -64,3 +65,8 @@ VALIDATE $? "Copying nginx.conf"
 
 systemctl restart nginx 
 VALIDATE $? "Restarting nginx"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
